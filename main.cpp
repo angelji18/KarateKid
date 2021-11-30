c37b4d2adac3c98caa44ef4bfee7284b650f3290
angelji18 <angelji@g.clemson.edu>
2021-11-29 19:40:10 -0500

    Game Pause added

--- main.cpp
+++ main.cpp
@@ -22,7 +22,11 @@ int main(){
   while(gameEngine->running()){
 
 
-
+    //currentTime holds the game clock time and is only incremented when the game is not paused
+    //the game is paused and resumed using the esc key
+    //a variable "esccount" declared in the GameEngine counts the number of times the esc key is pressed.
+    //initallly it is set to 0 and on the 1st click it will be set to 1 which will cause the pause screen to appear and the gameClock(currentTime) to stop from further incrementing(since it will cause paused=true)
+    //when the second esc key press in detected the esccount is set to 0 which caused the pause screen to disappear and the gameClock to resume
     if(!paused){
 
       deltaTime = SDL_GetTicks() / 1000.0f - lastTime; // time since last update
@@ -30,9 +34,11 @@ int main(){
       lastTime = SDL_GetTicks() / 1000.0f;
     }
     else{
-      lastTime = SDL_GetTicks() / 1000.0f;
+      lastTime = SDL_GetTicks() / 1000.0f; //for when the game is paused, we need the last value of the clock in the paused menu
+                                          // so here this value is overwritten and lastTime by the end of the pause cyle hold the latest value
     }
 
+    std::cout << currentTime << '\n'; //testing the clock value
     frameStart = SDL_GetTicks();
     gameEngine->handleGameEngineEvents();
     gameEngine->updateGameEngine(cameraRect);
@@ -42,7 +48,7 @@ int main(){
     if(frameDelay > frameTime){
       SDL_Delay(frameDelay - frameTime);
     }
-    std::cout << currentTime << '\n';
+
   }
 
   gameEngine->cleanGameEngine();
